e14c0125e03da431c1b2be3759ce57c32c49f45d
cze <abelleraczernycrille05@gmail.com>
2025-03-08 18:01:11 +0800

    Revert "UPDATE SHOP"
    This reverts commit e4f2c51cfcfcc7ff18cb6f0d641206910c3a24c0.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CssBaseline, Container, Box } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Navbar from "./components/Navbar";
-import Shop from "./pages/Shop";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -19,9 +18,7 @@ function App() {
         <Box sx={{ display: "flex", flexDirection: "column", height: "100vh", width: "100vw" }}>
           <Container maxWidth={false} sx={{ flexGrow: 1 }}>
             <Routes>
-            <Route path="/" element={<Dashboard />} />  {/* Dashboard route corrected */}
-              <Route path="/shop" element={<Shop />} />  {/* Home route corrected */}
-             
+              <Route path="/" element={<Dashboard />} />
               <Route path="/login" element={<Login />} />
               <Route path="/signup" element={<Signup />} />
             </Routes>
